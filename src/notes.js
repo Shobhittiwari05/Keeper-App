36b0c7e81b502a490cf60dc742db6bb0d5ee3ce0
Shobhittiwari05 <118071921+Shobhittiwari05@users.noreply.github.com>
2023-02-23 18:48:18 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,33 @@
+const notes = [
+  {
+    key: 1,
+    title: "VIRAT",
+    content:
+      "Q. How many players like virat very much? A. (2) Dhoni and Devilliers "
+  },
+  {
+    key: 2,
+    title: "Rohit",
+    content:
+      "Q.What make rohit special?.Ans) His 3 double centuries in odi that make rohit as hitman sharma "
+  },
+  {
+    key: 3,
+    title: "AB Devilliers",
+    content:
+      "The only player in cricket whose nickname is Mr360 as he play his shots in all area."
+  },
+  {
+    key: 4,
+    title: "Dhoni",
+    content:
+      "The most popular innings of Dhoni came in 2011 world cup. Mahi finishes off his style what a massive six!"
+  },
+  {
+    key: 5,
+    title: "Fitness",
+    content: "Learn from cricketer"
+  }
+];
+
+export default notes;
